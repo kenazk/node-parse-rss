69be14f487456cdb07d19a0f38f0f151f59669b0
Kenaz Kwa <kenazk@kenaz2.local>
2017-02-22 09:40:01 -0800

    Updating RSS feeds

---
+++ main.js
@@ -0,0 +1,28 @@
+var feed = require('feed-read'),  // require the feed-read module
+    http = require("http"),
+    port = process.env.PORT || 5000, // allow heroku/nodejitsu to set port
+    urls = [
+        "http://fetchrss.com/rss/58191cb88a93f87f4ee0dfdd22358496719.atom"
+    ]; // Example RSS Feeds
+
+// load css styles
+var css = '<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Raleway"> ';
+css = css + '<style type="text/css">' +require('fs').readFileSync('./style.css').toString() + '</style>'
+
+http.createServer(function (req, res) {
+  // send basic http headers to client
+  res.writeHead(200, {
+      "Content-Type": "text/html",
+      "Transfer-Encoding": "chunked"
+  });
+  // setup simple html page:
+  res.write("<html>\n<head>\n<title>Moravian Daily Text</title>\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />" +css +"</head>\n<body>");
+
+
+
+  setTimeout(function() {
+    res.end("</body>\n</html>"); // end http response
+  }, 4000);
+
+}).listen(port);
+console.log("HTTP Listening on: http://localhost:"+port);
